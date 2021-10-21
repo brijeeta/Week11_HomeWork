813781e9bd8fabd599a700bfc4d5d82afd6c05d4
Holy Paul <brijeeta@gmail.com>
2021-10-20 21:25:14 -0400

    added data,routes and server.js

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,30 @@
+// require variables
+const noteData = require('../data/noteData');
+
+// export app routes
+module.exports = (app) => {
+    // get notes from data array
+    app.get('/api/notes', (req, res) => res.json(noteData));
+
+    // post notes 
+    app.post('/api/notes', (req, res) => {
+        // add an id to the note
+        let id = noteData.length + 1;
+        req.body.id = parseInt(id);
+        // push note to data array
+        noteData.push(req.body);
+        // end response
+        res.json(true);
+    })
+
+    // delete notes
+    app.delete('/api/notes/:id', (req, res) => {
+        // identify the selected id
+        let note = noteData.find(({ id }) => id === JSON.parse(req.params.id));
+        // remove from data array
+        noteData.splice(noteData.indexOf(note), 1);
+        // end response
+        res.end();
+
+    })
+}
---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => console.log("Server is running on " + PORT));
